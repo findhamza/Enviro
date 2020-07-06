2afc36676e1256ba7d450183d482e7d174f80f37
findhamza <hxs034@shsu.edu>
2020-07-05 21:15:49 -0500

    Restructured project
    restructured project to make way for other resources that I need to learn and implement

---
+++ Lion_Legacy/cmake-build-debug/CMakeFiles/Display.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Display.dir/src/Testing/SDL/Display.cpp.obj"
+  "Display.exe"
+  "Display.exe.manifest"
+  "Display.pdb"
+  "libDisplay.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Display.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lion_Legacy/src/Testing/SDL/Display.h
@@ -0,0 +1,20 @@
+//
+// Created by Hamza Super PC on 6/15/2020.
+//
+
+#ifndef FSM_DISPLAY_H
+#define FSM_DISPLAY_H
+//Using SDL and standard IO
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+class Display {
+
+};
+
+
+#endif //FSM_DISPLAY_H
