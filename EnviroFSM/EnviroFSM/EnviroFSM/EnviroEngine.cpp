367ce68e15b856b94e7c504101379922d735c9f9
Hamza Syed <iamhamza@live.com>
2020-09-11 22:05:32 -0500

    completed rotating square
    Rotating square completed with basic shading. Next step is to work on genereating plant vertices

--- EnviroFSM/EnviroFSM/EnviroFSM/EnviroEngine.cpp
+++ EnviroFSM/EnviroFSM/EnviroFSM/EnviroEngine.cpp
@@ -110,12 +110,15 @@ void EnviroEngine::update()
         xFloat,  -yFloat, 0.0f,     xFloat,  -yFloat, 0.0f,  // top right       0
         xoFloat, -yoFloat, 0.0f,    xoFloat, -yoFloat, 0.0f,  // bottom right  1
         -xoFloat, yoFloat, 0.0f,    -xoFloat, yoFloat, 0.0f,  // bottom left   2
-        -xFloat,  yFloat, 0.0f,     -xFloat,  yFloat, 0.0f  // top left        3
+        -xFloat,  yFloat, 0.0f,     -xFloat,  yFloat, 0.0f,  // top left        3
+        0.0f, 0.0f, 0.0f,           0.0f, 0.0f, 0.0f 
     };
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+        1, 2, 3,    // second triangle
+        4, 0, 2
     };
+    indicesCount = sizeof(indices);
 
     glGenBuffers(1, &EBO);
 
@@ -156,7 +159,7 @@ void EnviroEngine::render()
     glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
     //glDrawArrays(GL_TRIANGLES, 0, 3);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, 0);
     // glBindVertexArray(0); // no need to unbind it every time 
 
     glDeleteBuffers(1, &VBO);
--- EnviroFSM/EnviroFSM/EnviroFSM/EnviroEngine.h
+++ EnviroFSM/EnviroFSM/EnviroFSM/EnviroEngine.h
@@ -23,18 +23,6 @@ class EnviroEngine
 	std::string fragShader = getShader("shader.frag");
 	const char* vShader = vertShader.c_str();
 	const char* fShader = fragShader.c_str();
-	/*const char* vShader = "#version 330 core\n"
-							"layout (location = 0) in vec3 aPos;\n"
-							"void main()\n"
-							"{\n"
-							"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-							"}\0";
-	const char* fShader =	"#version 330 core\n"
-							"out vec4 FragColor;\n"
-							"void main()\n"
-							"{\n"
-							"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-							"}\n\0";*/
 
 
 	int vertexShader;
@@ -42,6 +30,7 @@ class EnviroEngine
 	char infoLog[512];
 	int fragmentShader;
 	int shaderProgram;
+	int indicesCount;
 	unsigned int VBO, VAO, EBO;
 	float xFloat = 0, yFloat = 0, rFloat = 0.5, thetaFloat = 0;
 
