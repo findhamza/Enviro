6371ed510a819c5b145128df8d1d7bfdc6099871
Hamza Syed <iamhamza@live.com>
2020-11-15 23:12:01 -0600

    static to dynamic
    changed static buffer writes to dynamic, for every bit of performance boost. might save on memory a bit more

--- EnviroFSM/EnviroFSM/EnviroFSM/Mesh.h
+++ EnviroFSM/EnviroFSM/EnviroFSM/Mesh.h
@@ -80,11 +80,11 @@ private:
         // A great thing about structs is that their memory layout is sequential for all its items.
         // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
         // again translates to 3/2 floats which translates to a byte array.
-        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_DYNAMIC_DRAW);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         //glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW); //looks really beautiful but creates reflections
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size(), indices.data(), GL_STATIC_DRAW); //similar to old code (-1 to stop draw glitch.)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size(), indices.data(), GL_DYNAMIC_DRAW); //similar to old code (-1 to stop draw glitch.)
 
         // set the vertex attribute pointers
         // vertex Positions
@@ -100,7 +100,7 @@ private:
     {
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         //glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW); //looks really beautiful but creates reflections
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size(), indices.data(), GL_STATIC_DRAW); //similar to old code (-1 to stop draw glitch.)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size(), indices.data(), GL_DYNAMIC_DRAW); //similar to old code (-1 to stop draw glitch.)
     }
 };
 #endif
